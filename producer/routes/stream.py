0480571f50b31b7765cc40b291963ae31d3bfb3b
ansaha <ansaha@redhat.com>
2026-05-31 19:59:09 -0700

    adding comments to generator function for understanding

--- producer/routes/stream.py
+++ producer/routes/stream.py
@@ -10,7 +10,9 @@ from generator import generate_random_event
 router = APIRouter()
 
 
-async def generate_stream():
+async def generate_stream(): #power of async method. This will create the random event
+    #then it will asynchronously sleep for the interval and freeing up the thread for events to occur in the app
+    # then it will re-run the function
     interval = int(os.getenv("EVENT_INTERVAL_SECONDS", "2"))
     event = generate_random_event()
     if len(event_history) > MAX_HISTORY:
@@ -19,7 +21,8 @@ async def generate_stream():
     yield f"data: {event.model_dump_json()}\n\n"
     await asyncio.sleep(interval)
 
-@router.get('/stream')
+@router.get('/stream') #this will then infinitly call the function in a StreamingResponse which is a FASTAPI module
+#that will keep the http connection over to the consumer open while the streaming of data continues until the consumer closes the connection
 async def stream():
     return StreamingResponse(
         generate_stream(),
